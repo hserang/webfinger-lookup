85d03c03ebf1ddbbd81645bb3191f89e3715d9e5
Rod Calumpong <rod@ripple.com>
2014-10-23 15:40:13 -0700

    [TASK] Add wf model test and fixtures

--- test/fixtures/successes.js
+++ test/fixtures/successes.js
@@ -1,120 +1,31 @@
 var data = {
-  "https://latambridgepay.com": {
-    "subject": "https://latambridgepay.com",
-    "expires": "2014-01-30T09:30:00Z",
-    "properties": {
-      "name": "Latam Bridge Pay",
-      "description": "Ripple Gateway to and from Latin American banks.",
-      "rl:type": "gateway",
-      "rl:domain": "latambridgepay.com",
-      "rl:accounts": [
-        {
-          "address": "r4tFZoa7Dk5nbEEaCeKQcY3rS5jGzkbn8a",
-          "rl:currencies": [
-            "USD",
-            "BRL",
-            "PEN",
-            "MXN"
-          ]
-        }
-      ],
-      "rl:hotwallets": [
-        "rEKuBLEX2nHUiGB9dCGPnFkA7xMyafHTjP"
-      ]
-    },
-    "links": [
+  mockJSON: {
+    expires: "2014-10-23T19:10:57.290Z",
+    aliases: [
+      "ripple:r4tFZoa7Dk5nbEEaCeKQcY3rS5jGzkbn8a?dt=3"
+    ],
+    links: [
       {
-        "rel": "lrdd",
-        "template": "https://latambridgepay.com/.well-known/webfinger.json?q={uri}"
+        rel: "https://gatewayd.org/gateway-services/bridge_payments/quotes",
+        template: "https://staging.latambridgepay.com/vundefined{sender}/bridge_payments/quotes/{receiver}/{amount}",
+        properties: { }
       },
       {
-        "rel": "https://gatewayd.org/gateway-services/bridge_payments/quotes",
-        "template": "https://latambridgepay.com/v1/bridge_payments/quotes/{receiver}/{amount}",
-        "properties": {
-          "version": "1"
-        }
+        rel: "https://gatewayd.org/gateway-services/bridge_payments",
+        template: "https://staging.latambridgepay.com/vundefined/bridge_payments",
+        properties: { }
       },
       {
-        "rel": "https://gatewayd.org/gateway-services/bridge_payments",
-        "href": "https://latambridgepay.com/v1/bridge_payments",
-        "properties": {
-          "version": "1",
-          "fields": {
-            "sender_claims": {
-              "bank": {
-                "type": "AstropayBankCode",
-                "required": true,
-                "label": {
-                  "en": "Astropay Bank Code",
-                  "es": "Astropay Código del banco"
-                },
-                "description": {
-                  "en": "Bank code from Astropay's documentation"
-                }
-              },
-              "country": {
-                "type": "AstropayCountryCode",
-                "required": true,
-                "label": {
-                  "en": "Astropay Country Code"
-                },
-                "description": {
-                  "en": "Country code from Astropay's documentation"
-                }
-              },
-              "cpf": {
-                "type": "PersonalIDNumber",
-                "required": true,
-                "label": {
-                  "en": "Personal Government ID Number"
-                },
-                "description": {
-                  "en": "Personal ID number from the Astropay documentation"
-                }
-              },
-              "name": {
-                "type": "FullName",
-                "required": true,
-                "label": {
-                  "en": "Sender's Full Name"
-                },
-                "description": {
-                  "en": "First and last name of sender"
-                }
-              },
-              "email": {
-                "type": "EmailAddress",
-                "required": true,
-                "label": {
-                  "en": "Senders Email Address"
-                },
-                "description": {
-                  "en": "Sender's Email Address"
-                }
-              },
-              "bdate": {
-                "type": "Date",
-                "required": true,
-                "label": {
-                  "en": "YYYYMMDD"
-                },
-                "description": {
-                  "en": "Sender's date of birth"
-                }
-              }
-            }
-          }
-        }
-      },
-      {
-        "rel": "https://gatewayd.org/gateway-services/bridge_payment_status",
-        "template": "https://latambridgepay.com/api/v1/bridge_payments/{id}",
-        "properties": {
-          "version": "1"
-        }
+        rel: "https://gatewayd.org/gateway-services/bridge_payment_status",
+        template: "https://staging.latambridgepay.com/vundefined/bridge_payments/{id}",
+        properties: { }
       }
-    ]
-  }
+    ],
+    properties: { },
+    subject: "conner@staging.latambridgepay.com"
+  },
+  resource: "",
+  fullUrl: "https://staging.latambridgepay.com/.well-known/webfinger?resource=acct:conner@staging.latambridgepay.com"
 };
 
 module.exports = data;
---
+++ test/webfinger-model.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var chai = require('chai');
+var should = chai.should();
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
+var webfingerSuccess = require('./fixtures/successes');
+var webfingerError = require('./fixtures/errors');
+
+var Model = require('../app/scripts/stores/webfinger-model');
+
+var setUpSuccessfulModel = function() {
+  var url = 'latambridgepay.com';
+
+  this.model = new Model();
+  this.model.setResource('acct:conner@staging.latambridgepay.com');
+  this.model.setUrl('staging.latambridgepay.com');
+  this.model.set(webfingerSuccess.mockJSON);
+};
+
+var setUpErroneousModel = function() {
+  var url = '404';
+
+  this.model = new Model({url: url});
+  this.model.set(webfingerError[url]);
+};
+
+chai.should();
+chai.use(sinonChai);
+
+describe('invalid model', function() {
+  beforeEach(setUpErroneousModel);
+
+  it('should determine if model data is invalid', function() {
+    this.model.isValid().should.equal(false);
+  });
+});
+
+describe('valid model', function() {
+  beforeEach(setUpSuccessfulModel);
+
+  it('should determine if model data is valid', function() {
+    this.model.isValid().should.equal(true);
+  });
+});
+
+describe('required data', function() {
+  beforeEach(setUpSuccessfulModel);
+
+  it('should have correct url', function() {
+    this.model.url.should.exist
+      .and.equal(webfingerSuccess.fullUrl);
+  });
+
+  it('should have a subject', function() {
+    this.model.get('subject').should.exist
+      .and.be.a('string');
+  });
+
+  it('should have links', function() {
+    this.model.get('links').should.exist
+      .and.be.an('array').with.length.above(0);
+  });
+
+  it('should have an expiration date', function() {
+    this.model.get('expires').should.exist
+      .and.be.a('string');
+  });
+
+});
